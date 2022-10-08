211207cbae4392ee16d95dbf6c7e41195a4f923e
Jason Phelps <jnphelps02@gmail.com>
2022-10-08 16:33:20 +0100

    implemented hook for Accordion index

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -10,6 +10,8 @@ const items = [
 
 export default () => {
     return (
-        <Accordion items={items}/>
+        <div>
+            <Accordion items={items}/>
+        </div>
     );
 };
--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion = ({items}) => {
-    const renderedItems = items.map(item => {
-        return (<div key={item.title}> 
 
-            <div className="title active">
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    const renderedItems = items.map((item, index) => {
+        return (<React.Fragment key={item.title}> 
+
+            <div className="title active" onClick={() => onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
@@ -13,10 +20,13 @@ const Accordion = ({items}) => {
                 <p>{item.content}</p>
             </div>
 
-        </div>);
+        </React.Fragment>);
     });
 
-    return <div className="ui styled accordion">{renderedItems}</div>;
+    return <div className="ui styled accordion">
+        {renderedItems}
+        <h1>{activeIndex}</h1>
+    </div>;
 }
 
 export default Accordion;
