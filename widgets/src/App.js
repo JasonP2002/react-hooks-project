a048e3143f90f6e656c150538f614d9aa88ea997
Jason Phelps <jnphelps02@gmail.com>
2022-10-08 15:52:56 +0100

    added css for Accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,7 +3,7 @@ import Accordion from './components/Accordion';
 
 //Hard coded questions
 const items = [
-{title: 'What is React?', content: 'React us a front end JS framework.'}, 
+{title: 'What is React?', content: 'React is a front end JS framework.'}, 
 {title: 'Why use React?', content: 'React is a popular JS library.'}, 
 {title: 'How do you use React?', content: 'By creating components.'}
 ];
