706477ec7b39f13c04cbdbbd4653048d99a2d303
Jason Phelps <jnphelps02@gmail.com>
2022-10-08 15:42:48 +0100

    created Accordion component + display

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+//Hard coded questions
+const items = [
+{title: 'What is React?', content: 'React us a front end JS framework.'}, 
+{title: 'Why use React?', content: 'React is a popular JS library.'}, 
+{title: 'How do you use React?', content: 'By creating components.'}
+];
 
 export default () => {
-    return <h1>Widgets app</h1>;
+    return (
+        <Accordion items={items}/>
+    );
 };
