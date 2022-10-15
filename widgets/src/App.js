2a6ea0b247f9e4a9122767f4221b80a499034d33
Jason Phelps <jnphelps02@gmail.com>
2022-10-15 15:48:13 +0100

    added header component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -4,6 +4,7 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
 
 //Hard coded questions
 const accordianItems = [
@@ -23,6 +24,7 @@ export default () => {
 
     return (
         <div> 
+            <Header />
             <Route path="/"><Accordion items={accordianItems}/></Route>
             <Route path="/list"><Search /></Route>
             <Route path="/dropdown"><Dropdown label="Select a Colour" options={options} selected={selected} onSelectedChange={setSelected}/></Route>
